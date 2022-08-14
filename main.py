cafc0122b258c7667bee73487e86d28399019150
Andrew <62994051+Rarmash@users.noreply.github.com>
2022-08-15 02:50:57 +0300

    Code optimization

--- main.py
+++ main.py
@@ -2,16 +2,18 @@ from tkinter import *
 from tkinter import messagebox
 from tkinter import filedialog
 import webbrowser
+import src.fontslib as fontslib
+import src.themeslib as themeslib
 
 def change_theme(theme):
-    text_fild['bg'] = view_colors[theme]['text_bg']
-    text_fild['fg'] = view_colors[theme]['text_fg']
-    text_fild['insertbackground'] = view_colors[theme]['cursor']
-    text_fild['selectbackground'] = view_colors[theme]['select_bg']
+    text_fild['bg'] = themeslib.themes[theme]['text_bg']
+    text_fild['fg'] = themeslib.themes[theme]['text_fg']
+    text_fild['insertbackground'] = themeslib.themes[theme]['cursor']
+    text_fild['selectbackground'] = themeslib.themes[theme]['select_bg']
 
 
 def change_fonts(fontss):
-    text_fild['font'] = fonts[fontss]['font']
+    text_fild['font'] = fontslib.fonts[fontss]['font']
 
 
 def notepad_exit():
@@ -88,27 +90,6 @@ root.config(menu=main_menu)
 
 f_text = Frame(root)
 f_text.pack(fill=BOTH, expand=1)
-
-view_colors = {
-    'dark': {
-        'text_bg': 'black', 'text_fg': 'lime', 'cursor': 'brown', 'select_bg': '#8D917A'
-    },
-    'light': {
-        'text_bg': 'white', 'text_fg': 'black', 'cursor': '#A5A5A5', 'select_bg': '#FAEEDD'
-    }
-}
-
-fonts = {
-    'Arial': {
-        'font':'Arial 14 bold'
-    },
-    'CSMS': {
-        'font': ('Comic Sans MS', 14, 'bold')
-    },
-    'TNR': {
-        'font': ('Times New Roman', 14, 'bold')
-    }
-}
 
 text_fild = Text(f_text,
                  bg='white',
---
+++ src/fontslib.py
@@ -0,0 +1,11 @@
+fonts = {
+    'Arial': {
+        'font':'Arial 14 bold'
+    },
+    'CSMS': {
+        'font': ('Comic Sans MS', 14, 'bold')
+    },
+    'TNR': {
+        'font': ('Times New Roman', 14, 'bold')
+    }
+}
